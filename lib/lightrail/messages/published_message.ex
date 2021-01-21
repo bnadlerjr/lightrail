af8711cb1fb9e920fbd8393546a93404811ce097
Bob Nadler <bnadlerjr@gmail.com>
2021-01-21 08:17:15 -0500

    Schema and changeset for PublishedMessage

---
+++ lib/lightrail/messages/published_message.ex
@@ -0,0 +1,42 @@
+defmodule Lightrail.Messages.PublishedMessage do
+  @moduledoc """
+  Schema and helpers for a published message.
+
+  This is an internal module, not part of the public API.
+
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:uuid, :binary_id, autogenerate: false}
+
+  schema "lightrail_published_messages" do
+    field(:correlation_id, :binary_id)
+    field(:encoded_message, :string)
+    field(:exchange, :string)
+    field(:message_type, :string)
+    field(:status, :string)
+    field(:user_uuid, :binary_id)
+    timestamps()
+  end
+
+  def changeset(message, attrs) do
+    message
+    |> cast(attrs, permitted_fields())
+    |> validate_required([:encoded_message, :message_type, :status, :uuid])
+    |> unique_constraint(:uuid)
+  end
+
+  defp permitted_fields do
+    [
+      :correlation_id,
+      :encoded_message,
+      :exchange,
+      :message_type,
+      :status,
+      :user_uuid,
+      :uuid
+    ]
+  end
+end
