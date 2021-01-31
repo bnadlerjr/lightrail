6883fe831481aa19029bc4ce81c491f971250f13
Bob Nadler <bnadlerjr@gmail.com>
2021-01-31 09:51:14 -0500

    Forgot to fix tag in test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,7 +1,7 @@
-# RabbitMQ integration tests are slow and require a running RabbitMQ
+# Integration tests are slow and require a running RabbitMQ
 # instance so don't run them by default. You can run them using
-# `mix test --only rabbit`. CI will always run them.
-ExUnit.configure(exclude: [rabbit: true])
+# `mix test --only integration`. CI will always run them.
+ExUnit.configure(exclude: [integration: true])
 
 Test.Support.Repo.start_link()
 Ecto.Adapters.SQL.Sandbox.mode(Test.Support.Repo, :manual)
